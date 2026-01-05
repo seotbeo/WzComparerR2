f927d2774ab05b3c91ab3fccab985f6728e9e388
seotbeo <yoonv72@gmail.com>
2026-01-05 15:46:40 +0900

    Update NpcTooltipRenderer.cs

--- WzComparerR2/CharaSimControl/NpcTooltipRenderer.cs
+++ WzComparerR2/CharaSimControl/NpcTooltipRenderer.cs
@@ -236,7 +236,7 @@ namespace WzComparerR2.CharaSimControl
             {
                 int requiredLines = (int)Math.Ceiling(bitmaps.Count / (double)perLineCount);
 
-                int width = 0;
+                int width = 150;
                 int height = 0;
 
                 int currentLineWidth = 0;
@@ -302,7 +302,7 @@ namespace WzComparerR2.CharaSimControl
             else
             {
                 Bitmap targetIllust = bitmaps[npcIndex];
-                Bitmap result = new Bitmap(targetIllust.Width + 30, targetIllust.Height + 60, PixelFormat.Format32bppArgb);
+                Bitmap result = new Bitmap(Math.Max(targetIllust.Width, 150) + 30, targetIllust.Height + 60, PixelFormat.Format32bppArgb);
                 using (Graphics g = Graphics.FromImage(result))
                 {
                     GearGraphics.DrawNewTooltipBack(g, 0, 0, result.Width, result.Height);
