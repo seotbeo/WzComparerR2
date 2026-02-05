5b77a9cc30ce4f4a15c77640aea1a722ec1180f8
seotbeo <yoonv72@gmail.com>
2026-02-06 01:19:24 +0900

    StringLinker: fix KeyNotFoundException

--- WzComparerR2.Common/StringLinker.cs
+++ WzComparerR2.Common/StringLinker.cs
@@ -490,6 +490,10 @@ namespace WzComparerR2.Common
                                                         }
                                                         foreach (var mobID in mobIDs)
                                                         {
+                                                            if (!stringMob.ContainsKey(mobID))
+                                                            {
+                                                                continue;
+                                                            }
                                                             StringResult strResult = new StringResult();
                                                             strResult.Name = stringMob[mobID].Name;
                                                             strResult.Desc = desc;
@@ -513,6 +517,10 @@ namespace WzComparerR2.Common
                                                         }
                                                         foreach (var npcID in npcIDs)
                                                         {
+                                                            if (!stringNpc.ContainsKey(npcID))
+                                                            {
+                                                                continue;
+                                                            }
                                                             StringResult strResult = new StringResult();
                                                             strResult.Name = stringNpc[npcID].Name;
                                                             strResult.Desc = desc;
