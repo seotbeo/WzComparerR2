33de3d31709a19788d405b52035130cd9069b8d3
seotbeo <yoonv72@naver.com>
2026-05-10 15:53:30 +0900

    MapRender: support moveNode

--- WzComparerR2.MapRender/MeshBatcher.cs
+++ WzComparerR2.MapRender/MeshBatcher.cs
@@ -135,6 +135,8 @@ namespace WzComparerR2.MapRender
                 rect.X = -rect.Right;
             }
 
+            var alpha = mesh.Alpha < 255 ? (int)(frame.A0 * (mesh.Alpha / 255f)) : frame.A0;
+
             //兼容平铺
             if (mesh.TileRegion != null)
             {
@@ -155,7 +157,7 @@ namespace WzComparerR2.MapRender
                         Prepare(frame.Blend ? ItemType.Sprite_BlendAdditive : ItemType.Sprite);
                         sprite.Draw(frame.Texture, pos,
                             frame.AtlasRect,
-                            new Color(Color.White, frame.A0),
+                            new Color(Color.White, alpha),
                             0,
                             origin,
                             1,
@@ -173,7 +175,7 @@ namespace WzComparerR2.MapRender
                     Prepare(frame.Blend ? ItemType.Sprite_BlendAdditive : ItemType.Sprite);
                     sprite.Draw(frame.Texture, mesh.Position,
                         frame.AtlasRect,
-                        new Color(Color.White, frame.A0),
+                        new Color(Color.White, alpha),
                         0,
                         origin,
                         1,
@@ -652,6 +654,7 @@ namespace WzComparerR2.MapRender
                 mesh.Position = Vector2.Zero;
                 mesh.Z0 = 0;
                 mesh.Z1 = 0;
+                mesh.Alpha = 255;
                 mesh.FlipX = false;
                 mesh.TileRegion = null;
                 mesh.TileOffset = Vector2.Zero;
