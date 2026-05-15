657c675bf842a154ba2d6f24d6513dd0bc3d4ba3
seotbeo <yoonv72@gmail.com>
2026-05-15 12:12:03 +0900

    Update MainForm.Designer.cs

--- WzComparerR2/MainForm.Designer.cs
+++ WzComparerR2/MainForm.Designer.cs
@@ -3478,7 +3478,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(750, 513);
             this.Name = "MainForm";
-            this.Text = "WzComparerR2";
+            this.Text = "WzComparerR2-KMS";
             this.Shown += new System.EventHandler(MainForm_Shown);
             this.ribbonControl1.ResumeLayout(false);
             this.ribbonControl1.PerformLayout();
